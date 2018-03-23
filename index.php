d0cf518feebe3efe1a5773a46498f032266d3c1a
tongwen <602943077@qq.com>
2018-03-23 09:51:22 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="./js/vue.min.js"></script>
+</head>
+<body>
+<?php 
+$dir='./project/';
+include_once $dir.'expression.php'; 
+
+?>
+
+</body>
+</html>
---
+++ project/expression.php
@@ -0,0 +1,14 @@
+<div id="expression">
+	{{5+5}}<br>
+</div>
+
+<script type="text/javascript">
+new Vue({
+	el:"#expression",
+	data:{
+		ok:true,
+		message:'vue',
+		id:1
+	}
+})
+</script>
